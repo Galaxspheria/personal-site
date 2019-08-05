9bb310eb9b9e7e399d5e5935d20d7b2469c931aa
Hunter Hancock <hunterghancock@gmail.com>
2019-08-04 20:42:26 -0700

    [feat] Favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,9 +4,32 @@ import Helmet from 'react-helmet'
 import Navbar from '../components/Navbar'
 import './all.sass'
 
+import favicon16 from '../img/favicon-16x16.png'
+import favicon32 from '../img/favicon-32x32.png'
+import favicon96 from '../img/favicon-96x96.png'
+
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Hunter Hancock" />
+    <Helmet title="Hunter Hancock"
+      link={[
+        {"rel": "icon", 
+          "type": "image/png", 
+          "sizes": "16x16",
+          "href": favicon16
+        },
+        {
+          "rel": "icon",
+          "type": "image/png",
+          "sizes": "32x32",
+          "href": favicon32
+        },
+        {
+          "rel": "icon",
+          "type": "image/png",
+          "sizes": "96x96",
+          "href": favicon96
+        }
+        ]}/>
     <Navbar />
     <div>{children}</div>
   </div>
